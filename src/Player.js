6e2646a5cac289f80d98966eeac65208c01fd827
captaindevb <captain.dev.b@gmail.com>
2019-10-05 12:56:22 +0900

    PureComponent로 성능 개선

--- src/Player.js
+++ src/Player.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {Counter} from "./Counter";
 
 
-//props.id 받아줘야하고, 함수 선언문을 넣어야 하기때문에 익명함수로 랩핑하기
-export const Player = (props) =>  (
-    <div className="player">
+//this.props.id 받아줘야하고, 함수 선언문을 넣어야 하기때문에 익명함수로 랩핑하기
+export class Player extends React.PureComponent { // 내꺼만 렌더링
+
+    render() {
+        console.log(this.props.name, ' rendered');
+        return (
+            <div className="player">
         <span className="player-name">
             {/*//3. 콜백 펑션 호출 */}
-            <button className="remove-player" onClick={() => {props.removePlayer(props.id)}}>X</button>
-            {props.name}
+            <button className="remove-player" onClick={() => {this.props.removePlayer(this.props.id)}}>X</button>
+            {this.props.name}
         </span>
-        <Counter score={props.score} changeScore={props.changeScore} id={props.id}/>
-    </div>
-);
+                <Counter score={this.props.score} changeScore={this.props.changeScore} id={this.props.id}/>
+            </div>
+        )
+    }
+}
