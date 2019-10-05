f866d55246b962ad1ad46865055bd816be1ae811
captaindevb <captain.dev.b@gmail.com>
2019-10-05 13:15:36 +0900

    Header, Player 객체할당문법 적용

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,12 @@ import {Stats} from "./Stats";
 import {Stopwatch} from "./Stopwatch";
 
 //Header란 이름을 갖고 쓸 수 있게 배포하기 . 앞에 export 붙여서 named export 후 App.js에서 추가해줌.
-export const Header = (props) => {
-    console.log(props);
+export const Header = ({players, title}) => {
+    //const {players, title} = props;
     return (
         <header className="header">
-            <Stats players={props.players} />
-            <h1 className="h1">{props.title}</h1>
+            <Stats players={players} />
+            <h1 className="h1">{title}</h1>
             <Stopwatch></Stopwatch>
         </header>
     ); // 가독성을 위해 ()로 감싸기
--- src/Player.js
+++ src/Player.js
@@ -6,16 +6,20 @@ import {Counter} from "./Counter";
 export class Player extends React.PureComponent { // 내꺼만 렌더링
 
     render() {
-        console.log(this.props.name, ' rendered');
+        const {removePlayer, id, name, score, changeScore} = this.props;
         return (
             <div className="player">
         <span className="player-name">
             {/*//3. 콜백 펑션 호출 */}
-            <button className="remove-player" onClick={() => {this.props.removePlayer(this.props.id)}}>X</button>
-            {this.props.name}
+            <button className="remove-player" onClick={() => {removePlayer(id)}}>X</button>
+            {name}
         </span>
-                <Counter score={this.props.score} changeScore={this.props.changeScore} id={this.props.id}/>
+                <Counter score={score} changeScore={changeScore} id={id}/>
             </div>
         )
+    }
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(nextProps);
+        return nextProps.score !== this.props.score ? true: false;
     }
 }
